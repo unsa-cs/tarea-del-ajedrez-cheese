611eb72e93a12064c34a8d455b58b6d020530ee3
Marcos <maceituno@unsa.edu.pe>
2024-10-04 17:44:18 -0500

    Quisimos dibujar la primera fila, usando repeatH, nos dio error ya que se necesitaba un segundo argumento de tipo entero. Pensamos unir los cuadrados con repeatH y superImpose, aunque consumiera mas espacio de memoria, sin percatarnos que existia join, el cual finalmemte usamos. Con ayuda de repeatH y join se pudo crear lo solicitado

---
+++ prob01.c
@@ -0,0 +1,19 @@
+/*Primero probamos las funciones dadas en el documento, y se realizo como primera instancia, un cuadrado de color blanco
+  despues usando la funcion reverse se crear un cuadrado de color negro. Seguidamente usando la funcion join se unieron los dos
+  cuadrados de ambos colores. Finalmente usando la funcion repeatH se repitio 4 veces la imagen unida*/
+
+#include "chess.h"
+#include "figures.h"
+/*
+void display(){
+  char** square = reverse(whiteSquare);
+  interpreter(square);
+}*/
+void display(){
+  char** wSquare = whiteSquare;
+  char** bSquare = reverse(whiteSquare);
+  char** jSquare = repeatH(join(wSquare, bSquare),4);
+
+  interpreter(jSquare);
+
+}
