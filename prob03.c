14ad58cc637c331711179c1077ae8f0e4cd27c6b
Aleexs <aescarza@unsa.edu.pe>
2024-10-04 22:49:16 -0500

    Con la funcion up y reverse se dibujo dos filas con los casilleros intercalados en cada posicion

--- prob03.c
+++ prob03.c
@@ -6,11 +6,8 @@ void display(){
   char** wSquare = whiteSquare;
   char** bSquare = reverse(whiteSquare);
   char** horizontalSquare = repeatH(join(bSquare, wSquare),4);
-  char** reversoHorizontalSquare = reverse(horizontalSquare);
-  char** verticalSquare = repeatV(join(bSquare,wSquare),4);
-  char** reversoVerticalSquare = reverse(verticalSquare);
-  char** unirprueba = flipH(reversoHorizontalSquare);
-  char** unir = join(horizontalSquare,reversoHorizontalSquare);
-  interpreter(verticalSquare);
+  char** reversoHorizontalSquare = reverse(horizontalSquare);  
+  char** horizontalUp = up(horizontalSquare,reversoHorizontalSquare);  
+  interpreter(horizontalUp);
 }
 
