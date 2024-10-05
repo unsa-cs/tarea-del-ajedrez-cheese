9d5e01385462cba706204d402cd0381e8df5273e
AngelaSoto05 <asotohu@unsa.edu.pe>
2024-10-05 00:02:19 -0500

    Quisimos implementar todas las piezas uniendolas con la funcion join,solo nos falta de poner la funcion reverse para que sea de distinto color de casillero

--- prob04.c
+++ prob04.c
@@ -5,6 +5,13 @@ void display(){
   char** wSquare = whiteSquare;
   char** bSquare = reverse(whiteSquare);
   char** horizontalSquare = repeatH(join(bSquare, wSquare),4);
-  char** ficha = superImpose(bishop,horizontalSquare);
-  interpreter(ficha);
+  char** torre = superImpose(rook,horizontalSquare);
+  char** caballo =superImpose(knight,horizontalSquare);
+  char** alfil = superImpose(bishop,horizontalSquare);
+  char** reina = superImpose(queen,horizontalSquare);
+  char** rey =superImpose(king,horizontalSquare);
+  char** unirpiezas  =join(torre,caballo);
+
+
+  interpreter(unirpiezas);
 }
