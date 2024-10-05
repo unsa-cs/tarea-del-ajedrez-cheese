70eb07e0dab03654192f440de411a3e1a9746ba2
AngelaSoto05 <asotohu@unsa.edu.pe>
2024-10-05 00:24:27 -0500

    Unimos todo satisfactoriamente con la funcion join pero nos percatamos que nos olvidamos poner alfil2 ,caballo2 y torre2 ya que pintaban distinto el casillero y no daba con la imagen del problema

--- prob04.c
+++ prob04.c
@@ -5,13 +5,11 @@ void display(){
   char** wSquare = whiteSquare;
   char** bSquare = reverse(whiteSquare);
   char** horizontalSquare = repeatH(join(bSquare, wSquare),4);
-  char** torre = superImpose(rook,horizontalSquare);
-  char** caballo =superImpose(knight,horizontalSquare);
-  char** alfil = superImpose(bishop,horizontalSquare);
-  char** reina = superImpose(queen,horizontalSquare);
-  char** rey =superImpose(king,horizontalSquare);
-  char** unirpiezas  =join(torre,caballo);
-
-
+  char** torre = superImpose(rook,bSquare);
+  char** caballo =superImpose(knight,wSquare);
+  char** alfil = superImpose(bishop,bSquare);
+  char** reina = superImpose(queen,wSquare);
+  char** rey =superImpose(king,bSquare);
+  char** unirpiezas = join(join(join(join(torre,caballo),join(alfil,reina)),join(rey,alfil)),join(caballo,torre));
   interpreter(unirpiezas);
 }
