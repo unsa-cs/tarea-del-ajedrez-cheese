611571a4583e475ed1faa4b7e33ba1ec73e2d8ac
Juan Velasquez <jvelasquezmo@unsa.edu.pe>
2024-10-06 15:49:29 -0500

    Se consiguió completar la implementación de free() para liberar la memoria

--- prob07.c
+++ prob07.c
@@ -5,32 +5,26 @@
 void display(){
   char** wSquare = whiteSquare;
   char** bSquare = reverse(whiteSquare);
-
-  char** jSquare = join(bSquare,wSquare);
- // char** rSquare = repeatH(jSquare,1);
- // char** jrSquare = reverse(rSquare);
-  //char** rrSquare = up(rSquare,jrSquare);
-  //char** repetir= repeatV(rrSquare,1);  
- 
- 
+  char** jSquare = join(bSquare,wSquare); 
   char** tableroDos = up(jSquare, reverse(jSquare));
-
-  char** caballo1 = knight;
-  char** caballo2 = rotateL(caballo1);
+  char** caballo2 = rotateL(knight);
   char** caballo3 = rotateL(caballo2);
   char** caballo4 = rotateL(caballo3);
-
-  char** caballoArriba = join(caballo1, caballo2);
+  char** caballoArriba = join(knight, caballo2);
   char** caballoAbajo = join(caballo3, caballo4);
-
   char** unionCaballo = up(caballoArriba, caballoAbajo);
-
   char** superCaballo = superImpose(unionCaballo, tableroDos);
-  
-  //char** caballonegro= reverse(knight);
- // char** sobreponer= superImpose(knight,wSquare);
- // char** rotacaballo=rotateL(sobreponer);
- // char** union1=superImpose(rotacaballo, repetir);
   interpreter(superCaballo);
-  }
+  free(bSquare);
+  free(jSquare);
+  free(tableroDos);
+  free(caballo2);
+  free(caballo3);
+  free(caballo4);
+  free(caballoArriba);
+  free(caballoAbajo);
+  free(unionCaballo);
+  free(superCaballo);
+}
+
 
