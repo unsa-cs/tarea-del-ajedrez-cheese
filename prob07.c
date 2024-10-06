7d0bd3a6ffc4c0b0bc86840e9876302e8a9bf707
Juan Velasquez <jvelasquezmo@unsa.edu.pe>
2024-10-06 15:42:36 -0500

    Se realizó la implementación para un tablero de 2 x 2 con un caballo blanco rotado en 4 posiciones, aun falta gestionar la memoria adecuadamente

--- prob07.c
+++ prob07.c
@@ -7,14 +7,30 @@ void display(){
   char** bSquare = reverse(whiteSquare);
 
   char** jSquare = join(bSquare,wSquare);
-  char** rSquare = repeatH(jSquare,4);
-  char** jrSquare = reverse(rSquare);
-  char** rrSquare = up(rSquare,jrSquare);
-  char** repetir= repeatV(rrSquare,4);  
-  char** caballonegro= reverse(knight);
-  char** sobreponer= superImpose(knight,wSquare);
-  char** rotacaballo=rotateL(sobreponer);
-  char** union1=superImpose(repetir,rotacaballo);
-  interpreter(union1);
+ // char** rSquare = repeatH(jSquare,1);
+ // char** jrSquare = reverse(rSquare);
+  //char** rrSquare = up(rSquare,jrSquare);
+  //char** repetir= repeatV(rrSquare,1);  
+ 
+ 
+  char** tableroDos = up(jSquare, reverse(jSquare));
+
+  char** caballo1 = knight;
+  char** caballo2 = rotateL(caballo1);
+  char** caballo3 = rotateL(caballo2);
+  char** caballo4 = rotateL(caballo3);
+
+  char** caballoArriba = join(caballo1, caballo2);
+  char** caballoAbajo = join(caballo3, caballo4);
+
+  char** unionCaballo = up(caballoArriba, caballoAbajo);
+
+  char** superCaballo = superImpose(unionCaballo, tableroDos);
+  
+  //char** caballonegro= reverse(knight);
+ // char** sobreponer= superImpose(knight,wSquare);
+ // char** rotacaballo=rotateL(sobreponer);
+ // char** union1=superImpose(rotacaballo, repetir);
+  interpreter(superCaballo);
   }
 
