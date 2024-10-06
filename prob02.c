042ccc047790e5ed7870f9d0313175cc73b516d3
AngelaSoto05 <asotohu@unsa.edu.pe>
2024-10-06 10:56:44 -0500

    En el problema 02  hemos podido ver los errores que cuando se ejecuta free() en una variable que no recibe a una funcion nos aparece invalid pointer, tambien hemos visto bibliotecas como stdlib.h en donde pudimos ver que funcionaban correctamebte, la diferencia que tienen estas dos es:en que las dos se pùeden utilizar en el lenguaje c ,podemos ver que stdlib esta mas orientado a c y <cstdlib> es para generar una compatibilidad en c++

--- prob02.c
+++ prob02.c
@@ -1,9 +1,12 @@
 #include "chess.h"
 #include "figures.h"
-
+#include <stdlib.h>
 void display(){
   char** wSquare = whiteSquare;
   char** bSquare = reverse(whiteSquare);
   char** jinvertidoSquare = repeatH(join(bSquare, wSquare),4);
   interpreter(jinvertidoSquare);
+  free(bSquare);
+  free(jinvertidoSquare);
+
 }
